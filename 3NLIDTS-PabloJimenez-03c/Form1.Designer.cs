bbfdcb0ac804391ce2e0f2e5bf60765c133f7c71
T3RK0-1101 <2021pjg13@gmail.com>
2025-08-20 12:45:03 -0600

    Agregar archivos de proyecto.

---
+++ 3NLIDTS-PabloJimenez-03c/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace _3NLIDTS_PabloJimenez_03c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbcelcius = new System.Windows.Forms.TextBox();
+            this.tbfarenheit = new System.Windows.Forms.TextBox();
+            this.tbkelvin = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperatura ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celcius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Farenheit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kelvin";
+            // 
+            // tbcelcius
+            // 
+            this.tbcelcius.Location = new System.Drawing.Point(120, 78);
+            this.tbcelcius.Name = "tbcelcius";
+            this.tbcelcius.Size = new System.Drawing.Size(100, 22);
+            this.tbcelcius.TabIndex = 4;
+            // 
+            // tbfarenheit
+            // 
+            this.tbfarenheit.Location = new System.Drawing.Point(120, 114);
+            this.tbfarenheit.Name = "tbfarenheit";
+            this.tbfarenheit.Size = new System.Drawing.Size(100, 22);
+            this.tbfarenheit.TabIndex = 5;
+            // 
+            // tbkelvin
+            // 
+            this.tbkelvin.Location = new System.Drawing.Point(120, 151);
+            this.tbkelvin.Name = "tbkelvin";
+            this.tbkelvin.Size = new System.Drawing.Size(100, 22);
+            this.tbkelvin.TabIndex = 6;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(120, 195);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 7;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.tbkelvin);
+            this.Controls.Add(this.tbfarenheit);
+            this.Controls.Add(this.tbcelcius);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbcelcius;
+        private System.Windows.Forms.TextBox tbfarenheit;
+        private System.Windows.Forms.TextBox tbkelvin;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
